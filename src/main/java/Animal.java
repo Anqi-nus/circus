7411145cd3d8a2feacc491cb098a853cae69a913
anqi-nus <e0322837@u.nus.edu>
2020-02-08 15:48:32 +0800

    Edit code

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,6 +1,6 @@
-public class Animal {
+public abstract class Animal implements Asset{
     
-    public String speak(){
-        return null;
-    }
+    public abstract String speak();
+    // abstract means you dont have to add anything in this class. It can be incomplete.
+    // You can implement in the subclass instead
 }
